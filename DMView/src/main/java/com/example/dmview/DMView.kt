befa693d9cbfb77a5f2c9f9ea29c43db772a79a2
Anwesh43 <anweshthecool0@gmail.com>
2021-02-28 01:35:05 +0530

    adding DMView class

--- DMView/src/main/java/com/example/dmview/DMView.kt
+++ DMView/src/main/java/com/example/dmview/DMView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawDMNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawDM(scale, w, h, paint)
 }
+
+class DMView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
