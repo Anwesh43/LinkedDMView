3fedfe28da55cf6a48c9094fd8f1ff7da5213963
Anwesh43 <anweshthecool0@gmail.com>
2021-02-28 02:23:19 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkeddmview/MainActivity.kt
+++ app/src/main/java/com/example/linkeddmview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkeddmview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.dmview.DMView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DMView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
