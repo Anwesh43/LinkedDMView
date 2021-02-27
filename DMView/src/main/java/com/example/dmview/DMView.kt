5f6804f4faaeae956ca8056ebb81ce23d8c4b45d
Anwesh43 <anweshthecool0@gmail.com>
2021-02-28 01:23:41 +0530

    adding Canvas extension methods

--- DMView/src/main/java/com/example/dmview/DMView.kt
+++ DMView/src/main/java/com/example/dmview/DMView.kt
@@ -23,8 +23,39 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.5f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 30f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawDM(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    rotate(deg * sf4)
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 *  j)
+        drawLine(0f, 0f, size * 0.5f * sf1, 0f, paint)
+        drawLine(0f, 0f, -size * 0.5f * sf2, -size * 0.5f * sf2, paint)
+        drawLine(-size / 2, -size / 2, -size / 2 + size * sf3, -size / 2 + size * 0.5f * sf3, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawDMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawDM(scale, w, h, paint)
+}
